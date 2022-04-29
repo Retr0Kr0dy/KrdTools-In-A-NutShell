67d13bc555208df916eaf46484e12c843bf22838
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-04-30 00:27:23 +0200

    Create setup-cryptsis-rebirth.sh

---
+++ setup-cryptsis-rebirth.sh
@@ -0,0 +1,11 @@
+echo
+echo "Initialization | CryptSIS-rebirth"
+echo
+echo "  Cloning CryptSIS-rebirth..."
+echo
+git clone https://github.com/Retr0Kr0dy/CryptSIS-rebirth
+echo
+echo "  Creating CryptSIS-rebirth executable file in /home/KrdTools/"
+echo
+cp wifuck/CryptSIS-rebirth.py /home/KrdTools/cryptsis-rebirth
+chmod a+x /home/KrdTools/cryptsis-rebirth
