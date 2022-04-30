5c13da7378e01f43a07ed7509032d40cccb0406f
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-04-30 18:48:25 +0200

    Update setup-wifuck.sh

--- tools/setup-wifuck.sh
+++ tools/setup-wifuck.sh
@@ -7,5 +7,5 @@ git clone https://github.com/Retr0Kr0dy/wifuck tools/wifuck
 echo
 echo "  Creating WiFuck executable file in /home/KrdTools/"
 echo
-cp wifuck/WiFuck.py /bin/wifuck
+cp tools/wifuck/WiFuck.py /bin/wifuck
 chmod a+x /bin/wifuck
