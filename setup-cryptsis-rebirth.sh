ed93a3e7d8452ec8882c47c525906fa33dc7c28b
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-04-30 11:06:35 +0200

    Update setup-cryptsis-rebirth.sh

--- setup-cryptsis-rebirth.sh
+++ setup-cryptsis-rebirth.sh
@@ -7,5 +7,5 @@ git clone https://github.com/Retr0Kr0dy/CryptSIS-rebirth
 echo
 echo "  Creating CryptSIS-rebirth executable file in /bin"
 echo
-cp wifuck/CryptSIS-rebirth.py /bin/cryptsis-rebirth
+cp CryptSIS-rebirth/CryptSIS-rebirth.py /bin/cryptsis-rebirth
 chmod a+x /bin/cryptsis-rebirth
