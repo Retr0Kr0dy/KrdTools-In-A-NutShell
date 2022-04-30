7527e41a471c13f48ba0f2693f9ba608450d3d1e
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-04-30 11:31:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,45 @@
 
 clear
 echo "Kali In A Nutshell"
+
+#ALIAS
+
 echo
-echo "  | Creating /home/Krdtools directory ..."
+echo " adding ALIAS "
+echo
+alias ls='ls --color=auto'
+alias ip='ip --color=auto'
+alias grep='grep --color=auto'
+alias diff='diff --color=auto'
+alias lla='ls -la'
+
+#DEPS
+
+echo
+echo " adding DEPS "
+echo
+apt install python3 python3-pip netdiscover -y
+
+#TOOLS
+
+echo
+echo " adding TOOLS" 
+echo
+echo "  | Using /bin/..."
 echo
 echo "  Red Wifi"
 echo
 echo "  | Adding WiFuck ..."
 echo
 chmod u+x setup-wifuck.sh
-sudo ./setup-wifuck.sh
+./setup-wifuck.sh
 echo
 echo "  | Adding WiFuck-rpi ..."
 echo
 chmod u+x setup-wifuck-rpi.sh
-sudo ./setup-wifuck-rpi.sh
+./setup-wifuck-rpi.sh
 echo
 echo "  | Adding CryptSIS-rebirth ..."
 echo
 chmod u+x setup-cryptsis-rebirth.sh
-sudo ./setup-cryptsis-rebirth.sh
+./setup-cryptsis-rebirth.sh
