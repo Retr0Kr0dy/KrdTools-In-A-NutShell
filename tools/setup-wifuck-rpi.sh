175059d629f1b51a0f57c068db1025f66b6a3f69
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-05-05 09:45:21 +0200

    Update setup-wifuck-rpi.sh

--- tools/setup-wifuck-rpi.sh
+++ tools/setup-wifuck-rpi.sh
@@ -5,7 +5,7 @@ echo "  Cloning WiFuck..."
 echo
 git clone https://github.com/Retr0Kr0dy/wifuck tools/wifuck
 echo
-echo "  Creating WiFuck-rpi executable file in /bin"
+echo "  Creating WiFuck-rpi executable file in /usr/local/bin"
 echo
-cp tools/wifuck/WiFuck-rpi.py /bin/wifuck-rpi
-chmod a+x /bin/wifuck-rpi
+cp tools/wifuck/WiFuck-rpi.py /usr/local/bin/wifuck-rpi
+chmod a+x /usr/local/bin/wifuck-rpi
