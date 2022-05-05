26c2b7a686455aeb007bc1c64a783d95270057b0
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2022-05-05 09:45:05 +0200

    Update setup-cryptsis-rebirth.sh

--- tools/setup-cryptsis-rebirth.sh
+++ tools/setup-cryptsis-rebirth.sh
@@ -5,9 +5,9 @@ echo "  Cloning CryptSIS-rebirth..."
 echo
 git clone https://github.com/Retr0Kr0dy/CryptSIS-rebirth tools/CryptSIS-rebirth
 echo
-echo "  Creating CryptSIS-rebirth executable file in /bin"
+echo "  Creating CryptSIS-rebirth executable file in /usr/local/bin"
 echo
 chmod a+x tools/CryptSIS-rebirth/setup.sh
 ./tools/CryptSIS-rebirth/setup.sh
-cp tools/CryptSIS-rebirth/CryptSIS-rebirth.py /bin/cryptsis-rebirth
-chmod a+x /bin/cryptsis-rebirth
+cp tools/CryptSIS-rebirth/CryptSIS-rebirth.py /usr/local/bin/cryptsis-rebirth
+chmod a+x /usr/local/bin/cryptsis-rebirth
